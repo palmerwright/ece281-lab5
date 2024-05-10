2eb984f9c6a67c1a0392501ea057d45b83a16094
Wright <c26nicholas.wright@afacademy.af.edu>
2024-05-10 10:00:06 -0600

    final

--- src/hdl/ALU.vhd
+++ src/hdl/ALU.vhd
@@ -24,29 +24,81 @@
 --| ALU OPCODES:
 --|
 --|     ADD     000
---|
---|
---|
---|
+--|     SUB     001
+--|     OR      100
+--|     OR      101
+--|     AND     010
+--|     AND     011
+--|     L SHIFT 111
+--|     R SHIFT 110
 --+----------------------------------------------------------------------------
+
 library ieee;
   use ieee.std_logic_1164.all;
   use ieee.numeric_std.all;
 
 
 entity ALU is
--- TODO
+    port ( i_op     : in STD_LOGIC_VECTOR (2 downto 0);
+           i_A      : in STD_LOGIC_VECTOR (7 downto 0);
+           i_B      : in STD_LOGIC_VECTOR (7 downto 0);
+           
+           o_flag   : out STD_LOGIC_VECTOR (2 downto 0);
+           o_result : out STD_LOGIC_VECTOR (7 downto 0)
+         );
 end ALU;
 
 architecture behavioral of ALU is 
   
 	-- declare components and signals
-
+	signal w_posNeg        : STD_LOGIC_VECTOR (7 downto 0);
+	signal w_add           : STD_LOGIC_VECTOR (7 downto 0);
+	signal w_cOut          : STD_LOGIC;
+	signal w_and           : STD_LOGIC_VECTOR (7 downto 0);
+	signal w_or            : STD_LOGIC_VECTOR (7 downto 0);
+	signal w_rightShift    : STD_LOGIC_VECTOR (7 downto 0);
+	signal w_leftShift     : STD_LOGIC_VECTOR (7 downto 0);
+	signal w_shift         : STD_LOGIC_VECTOR (7 downto 0);
+	signal w_result        : STD_LOGIC_VECTOR (7 downto 0);
   
 begin
 	-- PORT MAPS ----------------------------------------
-
+	process(i_A, i_B, i_op)
+	begin
+	o_result <= i_A + i_B when (i_op = "000") else --addition
+	            i_A - i_B when (i_op = "001") else -- subtraction
+	            i_A or i_B when (i_op = "100" or i_op = "101") else -- or
+	            i_A and i_B when (i_op = "010" or i_op = "011") else -- and 
+	            STD_LOGIC_VECTOR(unsigned(i_A) sll i_B(2 downto 0)) when (i_op = "111") else -- left logical shift
+	            STD_LOGIC_VECTOR(unsigned(i_A) srl i_B(2 downto 0)); -- right logical shift
+	end process;
+	            
+    case i_op is
+                    when "001" =>
+                        o_result <= i_A - i_B;
+                        o_flag <= "010" when (i_A < i_B) else "000";
+                    when "100" | "101" =>
+                        o_result <= i_A or i_B;
+                        o_flag <= "000";
+                    when "010" | "011" =>
+                        o_result <= i_A and i_B;
+                        o_flag <= "000";
+                    when "111" =>
+                        o_result <= STD_LOGIC_VECTOR(unsigned(i_A) sll i_B(2 downto 0));
+                        o_flag <= "000";
+                    when "110" =>
+                        o_result <= STD_LOGIC_VECTOR(unsigned(i_A) srl i_B(2 downto 0));
+                        o_flag <= "000";
+                    when others =>
+                        o_result <= (others => '0');
+                        o_flag <= "000";
+                end case;
+                
+	    	    
 	
+    o_flag <= "100" when (i_op = "000") and ((i_A) + unsigned(i_B) > 255) else
+                          "000";
+
 	
 	-- CONCURRENT STATEMENTS ----------------------------
 	
--- src/hdl/top_basys3.vhd
+++ src/hdl/top_basys3.vhd
@@ -25,13 +25,84 @@ library ieee;
   use ieee.numeric_std.all;
 
 
-entity top_basys3 is
--- TODO
+entity top_basys3 is    
+    port(
+        -- inputs
+        clk     :   in std_logic; -- native 100MHz FPGA clock
+        sw      :   in std_logic_vector(7 downto 0);
+        btnU    :   in std_logic; -- reset
+        btnL    :   in std_logic; -- adv
+        
+        -- outputs
+        led :   out std_logic_vector(15 downto 0);
+        -- 7-segment display segments (active-low cathodes)
+        seg :   out std_logic_vector(6 downto 0);
+        -- 7-segment display active-low enables (anodes)
+        an  :   out std_logic_vector(3 downto 0)
+    );
 end top_basys3;
 
 architecture top_basys3_arch of top_basys3 is 
   
 	-- declare components and signals
+	
+	-- twoscomp_decimal
+	component twoscomp_decimal is
+        port (
+            i_binary: in std_logic_vector(7 downto 0);
+            o_negative: out std_logic;
+            o_hundreds: out std_logic_vector(3 downto 0);
+            o_tens: out std_logic_vector(3 downto 0);
+            o_ones: out std_logic_vector(3 downto 0)
+        );
+    end component twoscomp_decimal;
+    
+    -- controller_fsm
+    component controller_fsm is
+        port(
+            i_reset   : in std_logic;
+            i_adv     : in std_logic;
+            o_cycle   : out std_logic_vector (3 downto 0)
+          );
+    end component controller_fsm;
+    
+    
+    -- TDM4
+    component TDM4 is
+        generic ( constant k_WIDTH : natural  := 4); -- bits in input and output
+        Port ( i_clk        : in  STD_LOGIC;
+               i_reset        : in  STD_LOGIC; -- asynchronous
+               i_D3         : in  STD_LOGIC_VECTOR (k_WIDTH - 1 downto 0);
+               i_D2         : in  STD_LOGIC_VECTOR (k_WIDTH - 1 downto 0);
+               i_D1         : in  STD_LOGIC_VECTOR (k_WIDTH - 1 downto 0);
+               i_D0         : in  STD_LOGIC_VECTOR (k_WIDTH - 1 downto 0);
+               o_data        : out STD_LOGIC_VECTOR (k_WIDTH - 1 downto 0);
+               o_sel        : out STD_LOGIC_VECTOR (3 downto 0)    -- selected data line (one-cold)
+        );
+    end component TDM4;
+    
+    
+    -- clock_divider
+    component clock_divider is
+        generic ( constant k_DIV : natural := 2 ); -- How many clk cycles until slow clock toggles
+                                                   -- Effectively, you divide the clk double this 
+                                                   -- number (e.g., k_DIV := 2 --> clock divider of 4)
+        port (     i_clk    : in std_logic;
+                i_reset  : in std_logic;           -- asynchronous
+                o_clk    : out std_logic           -- divided (slow) clock
+        );
+    end component clock_divider;
+    
+    -- ALU
+    component ALU is
+        port ( i_op     : in STD_LOGIC_VECTOR (2 downto 0);
+               i_A      : in STD_LOGIC_VECTOR (7 downto 0);
+               i_B      : in STD_LOGIC_VECTOR (7 downto 0);
+               
+               o_flag   : out STD_LOGIC_VECTOR (2 downto 0);
+               o_result : out STD_LOGIC_VECTOR (7 downto 0)
+             );
+    end component ALU;
 
   
 begin
